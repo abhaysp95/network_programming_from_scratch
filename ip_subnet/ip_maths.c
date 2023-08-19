845e192d5693e4034410804164de0f9ba36f4bad
abhay <abhaysp95@protonmail.com>
2023-08-19 16:55:39 +0530

    feat: get ip integral equivalent

--- ip_subnet/ip_maths.c
+++ ip_subnet/ip_maths.c
@@ -8,43 +8,53 @@
 
 void get_broadcast_address(const char *, char, char *);
 void print_ip_bits(const char*);
+unsigned int get_ip_integral_equivalent(char*);
 
 int main(void) {
 	char ipaddr_buffer[PREFIX_LEN];
 	memset(ipaddr_buffer, '\0', PREFIX_LEN);
 	char *ip_addr = "192.168.2.1";
 	char mask = 20;
-	get_broadcast_address(ip_addr, mask, ipaddr_buffer);
-	printf("Broadcast addr = %s\n", ipaddr_buffer);
+	/* get_broadcast_address(ip_addr, mask, ipaddr_buffer);
+	printf("Broadcast addr = %s\n", ipaddr_buffer); */
+
+	printf("ip integral equivalent: %u\n", get_ip_integral_equivalent(ip_addr));
 
 	return 0;
 }
 
-// don't use int(4-byte or less) for storing ipv4, cause it'll work upto 2^31 not, 2^32
-void get_broadcast_address(const char *ip_addr, char mask, char *ipaddr_buffer) {
+// ip_buff should be of size 32
+void get_ip_bits(const char *ip_addr, char *ip_buff) {
 	char *temp_addr = (char *)malloc(strlen(ip_addr));
 	strncpy(temp_addr, ip_addr, strlen(ip_addr));
 	char *quad = strtok(temp_addr, ".");
 
-	char ip[32] = { 0 };
 	int counter = 24;
 
 	while (quad != NULL) {
 		int i = 0;
 		int q = atoi(quad);
 		while (q != 0) {
-			ip[counter + i++] |= (q & 1);
+			ip_buff[counter + i++] |= (q & 1);
 			q >>= 1;
 		}
 		counter -= 8;
 		quad = strtok(NULL, ".");
 	}
+	free(temp_addr);
+}
+
+// don't use int(4-byte or less) for storing ipv4, cause it'll work upto 2^31 not, 2^32
+void get_broadcast_address(const char *ip_addr, char mask, char *ipaddr_buffer) {
+	char ip_buf[32] = { 0 };
+	get_ip_bits(ip_addr, ip_buf);
+
 #ifdef DEBUG
 	print_ip_bits(ip);
 #endif
 
 	for (int i = 0; i < 32 - mask; i++) {
-		ip[i] = 1;
+		ip_buf[i] = 1;
 	}
 #ifdef DEBUG
 	print_ip_bits(ip);
@@ -52,15 +62,28 @@ void get_broadcast_address(const char *ip_addr, char mask, char *ipaddr_buffer) 
 
 	int num = 0;
 	int clen = 0;
-	counter = 0;
+	int counter = 0;
 	for (int i = 31; i >= 0; i--) {
-		num |= ip[i];
+		num |= ip_buf[i];
 		if (i % 8 == 0) {
 			clen += sprintf(ipaddr_buffer + clen, "%d", num);
 			if(counter++ < 3) ipaddr_buffer[clen++] = '.';
 			num = 0;
 		} else num <<= 1;  // shift only 7 times, not 8
 	}
+}
+
+unsigned int get_ip_integral_equivalent(char *ip_addr) {
+	char ip_buf[32] = { 0 };
+	get_ip_bits(ip_addr, ip_buf);
+
+	unsigned int ip = 0;
+	for (int i = 31; i >= 0; i--) {
+		ip |= ip_buf[i];
+		if(i != 0) ip <<= 1;
+	}
+
+	return ip;
 }
 
 void print_ip_bits(const char* ip) {
